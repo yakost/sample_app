1515ab82fb47cf64d3f2c45091f7cca1028eb95d
Your Name <your.email@example.com>
2016-05-07 14:48:13 +0300

    Finish layout and routes 3

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -56,6 +56,7 @@ describe "Static pages" do
    click_link "Home"
    click_link "Sign up now!"
    expect(page).to have_title(full_title('Sign up'))
-
+   click_link "sample app"
+   expect(page).to have_title(full_title(''))
  end
 end
